5479490388e39db1a3e01528df6eadffbefa8f16
Vladislav Diuzhev <vladdiuzhev@gmail.com>
2022-11-03 12:43:06 +0300

    Lidar model added.

--- controllers/lidar_python/lidar_python.py
+++ controllers/lidar_python/lidar_python.py
@@ -9,14 +9,14 @@ import numpy as np
 
 robot = Robot()
 
-map_ = np.zeros((5000,5000))
+map_ = np.ones((5000,5000))
 
 def add_obstacle(x, y, a, b):
     global map_
     x, y, a, b = map(int, [2500 + x*1000, 2500 - y*1000, a*500, b*500])
-    # print(x, y, a, b)
-    map_[y-b:y+b,x-a:x+a] = 1
-    
+    map_[y-b:y+b,x-a:x+a] = 0
+
+# Map creation
 add_obstacle(0,-1,0.01,0.5)
 add_obstacle(-0.5,-0.75,1,0.01)
 add_obstacle(0.5,-0.75,1,0.01)
@@ -28,6 +28,7 @@ add_obstacle(1,-1,0.01,0.5)
 add_obstacle(0,0.5,3,0.01)
 add_obstacle(1.5,1.5,0.5,0.5)
 
+# Robot sensors initialization
 lidar = robot.getDevice("Lidar")
 gps = robot.getDevice("gps")
 motor_l = robot.getDevice("left wheel motor")
@@ -35,9 +36,7 @@ motor_r = robot.getDevice("right wheel motor")
 sensor_l = robot.getDevice("left wheel sensor")
 sensor_r = robot.getDevice("right wheel sensor")
 
-
-timestep = 1 #int(robot.getBasicTimeStep())
-
+timestep = 1
 
 lidar.enable(timestep)
 gps.enable(timestep)
@@ -47,12 +46,44 @@ motor_l.setPosition(float('inf'))
 motor_r.setPosition(float('inf'))
 
 
+# Odometry and lidar modeling
+lidar_points_num = 3
+
 wheel_R = 0.02
 dist_between = 0.052
 odometry_state = np.array([0, 0, 0],dtype=float)
 pos_prev = [0, 0]
 
-def odometry_diff():
+def procceed_lidar(): # Retuns lidar_points_num distances
+    lidar_data = np.array(lidar.getRangeImage())
+    indexes = np.linspace(0,lidar_data.shape[0]-1,lidar_points_num,dtype=int)
+    return lidar_data[indexes]
+    
+def is_obst(x, y): # checks if there an obstacle
+    x, y= map(int, [ 2500 - y*1000, 2500 + x*1000])
+    return x < 0 or x >= map_.shape[0] or y < 0 or y >= map_.shape[1] or map_[x,y] == 0
+
+# takes robot state vector and lidar range (4m)
+# returns lidar model data
+def modelate_lidar(x, y, angle, ran): 
+    ang = np.linspace(angle + 120/180*np.pi,angle - 120/180*np.pi,N) # vision field
+    output = np.zeros_like(ang)
+    ang[ang<-np.pi] += 2*np.pi
+    ang[ang>-np.pi] -= 2*np.pi
+    for i, a in enumerate(ang):
+        x2, y2 = x + ran*np.cos(a), y + ran*np.sin(a)
+        steps= 10**4
+        for step in range(steps):
+            x_s = x + step*(x2-x)/steps
+            y_s = y + step*(y2-y)/steps
+            if is_obst(x_s,y_s):
+                output[i] = np.sqrt((x_s-x)**2 + (y_s-y)**2)
+                break
+        else:
+            output[i] = np.inf
+    return output
+
+def odometry_diff(): # calculates state difference with odometry
     global pos_prev
     pos_l = sensor_l.getValue()
     pos_r = sensor_r.getValue()
@@ -64,35 +95,15 @@ def odometry_diff():
     dx = (diff_l + diff_r)/2.0 * np.cos(odometry_state[2]+dtheta)
     dy = (diff_l + diff_r)/2.0 * np.sin(odometry_state[2]+dtheta)
     pos_prev = [pos_l, pos_r]
-    # print(dx,dy,dtheta)
     return np.array([dx,dy,dtheta])
 
-gps_data = np.zeros(2)
-gps_data_prev = np.zeros(2)
-angle = 0
-angle_prev = 0
+# Create sample 
+
+# 
 
 while robot.step(timestep) != -1:
-    gps_data = np.array(gps.getValues())[:-1]
-    
-    diff = gps_data - gps_data_prev
-    angle = np.arctan2(np.array(diff[1]),np.array(diff[0]))
-    
-    dangle = angle - angle_prev
-    rho = np.sqrt(np.sum(diff**2))
-    
     motor_l.setVelocity(5)
     motor_r.setVelocity(5)
     
-    print(angle*180/np.pi)
-    
-    # print(gps_data[0]/((sensor_l.getValue()+sensor_r.getValue())/2))
-    # print(odometry_state[-1]/np.pi*180)
-    # print(np.sqrt(np.sum((gps_data[:-1]-odometry_state[:-1])**2)))
-    # print(gps_data[:-1],odometry_state)
-    # odometry_state += odometry_diff()
+    print(procceed_lidar())
 
-    lidar_data = np.array(lidar.getRangeImage())
-    gps_data_prev[:] = gps_data[:]
-    agle_prev = angle
-    
